cda4534b597f15ffe04775f422cbca0e3d10dbe2
hannahlevy <hlevy@mymail.mines.edu>
2019-05-21 17:10:01 -0600

    changed add person query

--- thebridge/personnel/views.py
+++ thebridge/personnel/views.py
@@ -22,11 +22,12 @@ def submit(request):
         startDate = request.POST["time-start"]
         title = request.POST["title"]
         residency = request.POST["residency"]
-        newPer = "INSERT INTO person (id,fName,lName,title,residency) VALUES (%s, %s, %s, %s, %s)" % (empNum, firstName, lastName, title, residency)
-        newStat = "INSERT INTO person_status (status, date_change, person_id,) VALUES (%s, %s, %s)" % ("Active", startDate, empNum)
+        newPer = "INSERT INTO person (id,fname,lname,title,resident) VALUES ('%s', '%s', '%s', '%s', '%s');" % (empNum, firstName, lastName, title, residency)
+        newStat = "INSERT INTO person_status (status, date_change, person_id,) VALUES (%s, %s, %s);" % ("Active", startDate, empNum)
         run_query(newPer)
         run_query(newStat)
-        return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate + " " + title + " " + residency)
+       # return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate + " " + title + " " + residency)
+        return HttpResponse(newPer)
     except:
         return HttpResponse("Error adding data to the database")
 
