fc5e0ea731b7d2a46779dea2f32d6ef1a4706d1d
hannahlevy <hlevy@mymail.mines.edu>
2019-05-21 15:43:03 -0600

    Made forms work!

--- thebridge/personnel/views.py
+++ thebridge/personnel/views.py
@@ -7,3 +7,14 @@ def index(request):
     template = loader.get_template('personnel.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def submit(request):
+    try:
+        firstName = request.POST["firstname"]
+        lastName = request.POST["lastname"]
+        empNum = request.POST["empNumber"]
+        startDate = request.POST["time-start"]
+        return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate)
+    except:
+        return HttpResponse("Error adding data to the database")
--- thebridge/thebridge/settings.py
+++ thebridge/thebridge/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
