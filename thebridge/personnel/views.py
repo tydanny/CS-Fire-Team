29d5a7e80a3fc2aeff40ef375e32d6667b129f0e
Ty <tychristensen97@gmail.com>
2019-05-21 16:02:46 -0600

    Added employee list query

--- thebridge/personnel/views.py
+++ thebridge/personnel/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from dbconnect import run_query
 
 # Create your views here.
-def index(request):    
-    template = loader.get_template('personnel.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+def index(request):
 
+    empQuery = 'SELECT last, first FROM PERSON SORT BY last DESC;'
+
+    employees = run_query(empQuery)
+
+    template = loader.get_template('personnel.html')
+    context = {'employees' : employees}
+    return HttpResponse(template.render(context, request))
 
 def submit(request):
     try:
