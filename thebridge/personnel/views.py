f57bb6aa77d9bc49a2c29b926f74b1e004fe0de0
Connor Brennan <42980947+ConnorBrennan@users.noreply.github.com>
2019-05-21 17:05:26 -0600

    Recreated the update query

--- thebridge/personnel/views.py
+++ thebridge/personnel/views.py
@@ -24,7 +24,7 @@ def submit(request):
         title = request.POST["title"]
         residency = request.POST["residency"]
         newPer = ("INSERT INTO person (id,fName,lName,title,residency) VALUES (%s, %s, %s, %s, %s)" % (empNum, firstName, lastName, title, residency))
-        newStat = ("INSERT INTO person_status (status, date_change, person_id,) VALUES (%s, %s, %s)" % ("Active", startDate, empNum))
+        newStat = ("INSERT INTO person_status (status, date_change, person_id) VALUES (%s, %s, %s)" % ("Active", startDate, empNum))
         run_query(newPer)
         run_query(newStat)
         return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate + " " + title + " " + residency)
@@ -33,9 +33,11 @@ def submit(request):
 
 def update(request):
     try:
-        employee = request.POST["employee"]
+        #Set up for post-changes to update fields: They need to send employee numbers
+        empNum = request.POST["empNum"]
         status = request.POST["status"]
         date = request.POST["date"]
+        statusUpdate = ("INSERT INTO person_status (status, date, empNum) VALUES (%s, %s, %s)" % (status, date, empNum))
         return HttpResponse(employee + " " + status + " " + date)
     except:
         return HttpResponse("Error adding data to the database")
