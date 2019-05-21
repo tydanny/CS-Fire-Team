4367021662f1df7b6f0d5bd3afe495dfd7a44084
hannahlevy <hlevy@mymail.mines.edu>
2019-05-21 16:17:53 -0600

    Added handler for update

--- thebridge/personnel/views.py
+++ thebridge/personnel/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-#from dbconnect import run_query
+from dbconnect import run_query
 
 # Create your views here.
 def index(request):
@@ -24,8 +24,16 @@ def submit(request):
         residency = request.POST["residency"]
         newPer = "INSERT INTO person (id,fName,lName,title,residency) VALUES (%s, %s, %s, %s, %s)" % (empNum, firstName, lastName, title, residency)
         newStat = "INSERT INTO person_status (status, date_change, person_id,) VALUES (%s, %s, %s)" % ("Active", startDate, empNum)
-        #run_query(newPer)
-        #run_query(newStat)
-        return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate)
+        run_query(newPer)
+        run_query(newStat)
+        return HttpResponse(firstName + " " + lastName + " " + str(empNum) + " " + startDate + " " + title + " " + residency)
+    except:
+        return HttpResponse("Error adding data to the database")
+
+def update(request):
+    try:
+        employee = request.POST["employee"]
+        status = request.POST["status"]
+        return HttpResponse(employee + " " + status)
     except:
         return HttpResponse("Error adding data to the database")
