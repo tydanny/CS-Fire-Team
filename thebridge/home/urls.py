6918ca30d44a2a243a8e6990c797fcf55566197c
heidihufford <heidihufford@mymail.mines.edu>
2019-05-28 16:20:51 -0600

    Moved firefighter home link to example report

--- thebridge/home/urls.py
+++ thebridge/home/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:empNum>/', views.user, name='user'),
-	path('user', views.userhome, name='userhome'),
 	path('officer', views.officer, name='officer'),
 	path('admin', views.admin, name='admin'),
 ]
