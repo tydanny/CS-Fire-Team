0f432b126b9c0d96c03b4eb538c2c3a62c889774
heidihufford <heidihufford@mymail.mines.edu>
2019-05-28 15:22:26 -0600

    Removed unnecessary about page and began migrating user homepage code to user dashboard file

--- thebridge/about/urls.py
+++ thebridge/about/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
 	path('user', views.user, name='user'),
 	path('officer', views.officer, name='officer'),
 	path('admin', views.admin, name='admin'),
--- thebridge/about/views.py
+++ thebridge/about/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def index(request):
-    template = loader.get_template('about.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-	
 def user(request):
 	template = loader.get_template('user_about.html')
 	context = {}
