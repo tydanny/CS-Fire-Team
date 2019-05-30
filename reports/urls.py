c3df86dbe1a3008ca2989bfbd5ec7c0915712ce8
heidihufford <heidihufford@mymail.mines.edu>
2019-05-30 08:33:16 -0600

    Added submit and error files for reports and removed some hardcoded links

--- reports/urls.py
+++ reports/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='reports'),
-    path('submit/', views.submit, name='submit_report'),
+    path('submit/', views.submit, name='admin_submit'),
+	path('officer_submit/', views.officer_submit, name='officer_submit'),
 	path('officer', views.officer, name='officer_reports'),
 	path('admin', views.admin, name='admin_reports'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -17,6 +17,7 @@ def admin(request):
 	context = {}
 	return HttpResponse(template.render(context, request))
 
+
 def index(request):
     connection = dbconnect()
     firstQuery = "SELECT fname FROM PERSON;"
@@ -91,6 +92,62 @@ def submit(request):
         return response
     except Exception as e:
         print(e)
-        template = loader.get_template('error.html')
+        template = loader.get_template('admin_error.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+
+def officer_submit(request):
+    try:
+        startDate = request.POST["time-start"]
+        startTime = "%s 00:00:00.00" % startDate
+        endDate = request.POST["time-end"]
+        currTime = str(datetime.datetime.now().time())
+        endTime = "%s %s" % (endDate, currTime)
+        reportType = request.POST["type"]
+        staff = request.POST.getlist("staff")
+        connection = dbconnect()
+        reports = []
+        if staff[0] == "Generate For All":
+                numsQuery = "SELECT id FROM PERSON;"
+                nums = connection.s_query(numsQuery)
+                i = len(nums)
+                empNums = []
+                x = 0
+                while x < i:
+                        empNums.append(nums[x][0])
+                        x += 1
+                for emp in empNums:
+                    report = Report(str(emp), startTime, endTime)
+                    report.compute_full_report()
+                    reports.append(report)
+        elif len(staff) == 1:
+            nums = [int(s) for s in staff[0].split() if s.isdigit()]
+            empNum = nums[-1]
+            report = Report(str(empNum), startTime, endTime)
+            report.compute_full_report()
+            reports.append(report)
+        else:
+            empNums = []
+            for person in staff:
+                nums = [int(s) for s in person.split() if s.isdigit()]
+                empNums.append(nums[-1])
+            for emp in empNums:
+                report = Report(str(emp), startTime, endTime)
+                report.compute_full_report()
+                reports.append(report)
+        connection.close()
+        if len(reports) >= 1:
+            csv_name = "Personnel_Report_%s_%s.csv" % (startDate, endDate)
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="%s"' % csv_name
+            writer = csv.writer(response)
+            writer.writerow(reports[0].headerRow)
+            for report in reports:
+                    writer.writerow(report.csvRow)
+        return response
+    except Exception as e:
+        print(e)
+        template = loader.get_template('officer_error.html')
         context = {}
         return HttpResponse(template.render(context, request))
