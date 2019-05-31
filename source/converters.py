cd481875361c88a5818a989812f95dbacbe751d4
ConnorBrennan <cmbrennan@mymail.mines.edu>
2019-05-31 12:21:46 -0600

    Working on W2W converter function

--- source/converters.py
+++ source/converters.py
@@ -4,7 +4,7 @@ import lxml
 from dbconnect import dbconnect
 import json
 
-def convert_schedule(filepath):
+def convert_iar(filepath):
   
   db = dbconnect()
   #This should be dynamic, and it should function fine.
@@ -37,7 +37,32 @@ def convert_incidents(rep){
     e = WHATEVER THE FILE IS.get_exposure(id)
     
     type = e['incidentType']
-    
+    response = 0 #This will be a calculation in the future.
     db.load_incident(id, time, type, response)
   
   db.close
+  
+def convert_wtw(filepath):
+  db = dbconnect()
+  
+  rep = pd.read_csv(filepath)
+  
+  for index, row in rep[0].iterrows():
+    #This code is for the other function needs adjustment
+    
+    tstart = row.loc['Start date'].replace('/','-') + ' ' + row.loc['Start time'] + ':00'
+    tend = row.loc['End date'].replace('/','-') + ' ' + row.loc['End time'] + ':00'
+    location = row.loc['On duty at']
+    if(not db.get_shift(tstart, tend, location)):
+        print('Loc: ', location, 'Start time: ', tstart, 'End time: ', tend)
+        db.load_shift(tstart,tend,location)
+    role = row.loc['On duty for']
+    
+    #Should work for all, since i believe psycopg2 returns a lsit even if there is only one entry.  
+    #However, it will jsut take the first id if there are two people with the same name.
+    person = db.get_person_from_name(row.loc['First name'], row.loc['Last name'])[0][0]
+    if(not db.get_shift(tstart, tend, location)):
+      print(person)
+      db.load_person_xref_shift(tstart, tend, location, person, role)
+
+  db.close()
