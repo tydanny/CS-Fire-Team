dfa6959234198989fd7bf153cf74eb6430a320d4
Ty <tchristensen@mines.edu>
2019-06-04 14:42:59 -0600

    updated subscription keys for events in er

--- source/er.py
+++ source/er.py
@@ -13,7 +13,7 @@ def load_incidents(username=None, password=None, **kwargs):
     if username == None:
         username = input("Enter your username: ")
     
-    db = dbconnect()
+    db = dbconnect.dbconnect()
     
     access_token = get_token_pass(username, password)
     
@@ -292,7 +292,7 @@ def get_events(access_token=None, **kwargs):
 
     headers = {
         # Request headers
-        'Ocp-Apim-Subscription-Key': '1e9590cf0a134d4c99c3527775b03080',
+        'Ocp-Apim-Subscription-Key': 'dd47ea607c5648dc8c2677b5fe8c6126',
         'Authorization': access_token,
     }
 
@@ -329,7 +329,7 @@ def load_events(username=None, password=None, **kwargs):
 def get_event_people(event_type, access_token):
     headers = {
         # Request headers
-        'Ocp-Apim-Subscription-Key': '1e9590cf0a134d4c99c3527775b03080',
+        'Ocp-Apim-Subscription-Key': 'dd47ea607c5648dc8c2677b5fe8c6126',
         'Authorization': access_token,
     }
 
@@ -351,3 +351,42 @@ def get_event_people(event_type, access_token):
         return j["eventPeople"]
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def get_people(access_token=None, **kwargs):
+
+    if access_token == None:
+        access_token = get_token_pass(kwargs.get('username'), kwargs.get('password'))
+
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '1e9590cf0a134d4c99c3527775b03080',
+        'Authorization': access_token,
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters
+    })
+
+    try:
+        conn = http.client.HTTPSConnection('data.emergencyreporting.com')
+        conn.request("GET", "/agencyusers/users?%s" % params, headers=headers)
+        response = conn.getresponse()
+        data = response.read().decode()
+        conn.close()
+        return data
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def load_people(access_token=None, **kwargs):
+
+    if access_token == None:
+        access_token = get_token_pass(kwargs.get('username'), kwargs.get('password'))
+
+    users = get_people(access_token)
+    db = dbconnect.dbconnect()
+
+    ids = db.s_query("""
+    SELECT id FROM person;
+    """)
+    
