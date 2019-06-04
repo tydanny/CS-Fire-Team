f86e63a1057082a69ba99cf54aed98806034dad9
ConnorBrennan <cmbrennan@mymail.mines.edu>
2019-06-04 16:42:02 -0600

    Fixed an import error

--- import/views.py
+++ import/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.core.files.storage import FileSystemStorage
-from source import converters.py
+from source import converters
 
 
 # Create your views here.
@@ -16,6 +16,10 @@ def upload(request):
         myfile = request.FILES['fileToUpload']
         template = loader.get_template('submit.html')
         context = {}
+        
+        #I dont know if this works or if changes need to be made to account for django stuff
+        converters.convert_iar(myfile)
+        
         return HttpResponse(template.render(context, request))
     except:
         template = loader.get_template('error.html')
