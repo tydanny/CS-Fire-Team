484d1b951754569addafbf585b017887e8a3cd06
Ty <tchristensen@mines.edu>
2019-06-14 10:11:58 -0600

    removed unused files from source

--- source/person.py
+++
@@ -1,8 +0,0 @@
-
-class Person:
-  def __init__(self,id,fname,lname,title,resident):
-    self.id = id
-    self.fname = fname
-    self.lname = lname
-    self.title = title
-    self.resident = resident
